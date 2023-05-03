7592af8e3e1b6559d9a208f31864eca4ea81db6a
Sam Fred <samyung05@gmail.com>
2023-05-03 09:10:23 -0400

    commit

--- server/controls/auth.js
+++ server/controls/auth.js
@@ -37,7 +37,73 @@ const createSendToken = (user, statusCode, req, res) => {
 export const login = catchAsync(async (req, res, next) => {
     const {email, password} = req.body
 
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email })
 
     if(!existingUser) return next(new AppError('Invalid credentials', 400))
+    
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
+
+    if(!isPasswordCorrect) return next(new AppError('Invalid credentials', 400))
+
+    createSendToken(existingUser, 200, req, res)
+})
+
+export const forgotPassword = catchAsync(async (req, res, next) => {
+    const { email } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) return next(new AppError('Invalid email', 404))
+    
+    const resetToken = user.createResetPasswordToken()
+    await user.save()
+
+    try {
+        const resetUrl = `http://localhost:4000/passwordreset/${resetToken}`
+
+        const message = `
+       <h2>You had requested for a password reset</h2>
+       <p>Please click the following to reset your password:</p>
+       <a href=${resetURL} clicktracking=off>ResetLink</a>
+        `
+        await sendEmail({
+            to: user.email,
+            subject: 'Password Reset Request',
+            text: message
+        })
+
+        res.status(200).json({ success: true, data: 'message sent!'})
+    }
+    catch (error) {
+        console.log(error)
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+        await user.save()
+
+        return next(new AppError('Error sending email, please try again!', 500))
+    }
+})
+
+export const resetPassword = catchAsync(async (req, res, next) => {
+    const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resetToken)
+    .digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if(!user) return next(new AppError('Invalid Token', 400))
+    user.password = req.body.password
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save()
+
+    res.status(201).json({
+        success: true,
+        data: "Password Updated",
+    })
 })
---
+++ server/controls/product.js
@@ -0,0 +1,24 @@
+import Product from '../models/Product'
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
+export const getProductById = async (req, res) => {
+    const id = req.params.id
+    try {
+        const products = await Product.findById(id)
+        res.status(200).json(products)
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server Error '})
+    }
+}
