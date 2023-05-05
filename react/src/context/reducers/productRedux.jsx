a6ef8a3e9e0a6e076b84b8f5f37e8335f6dff69a
Sam Fred <samyung05@gmail.com>
2023-05-05 11:00:43 -0400

    create productRedux

---
+++ react/src/context/reducers/productRedux.jsx
@@ -0,0 +1,42 @@
+import * as actionTypes from '../constants/productConstants'
+
+const productReducer = (state, action) => {
+    switch (action.type) {
+        case actionTypes.GET_PRODUCT_REQUEST:
+            return {
+                products: [],
+                product: {},
+                isLoading: true,
+                error: false
+            }
+
+        case actionTypes.GET_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                products: [...state.product, ...action.payload],
+                product:{},
+                isLoading: false,
+                error: false
+            }
+
+        case actionTypes.GET_PRODUCT_DETAILS_SUCCESS:
+            return {
+                products: [],
+                product: action.payload,
+                isLoading: false,
+                error: false
+            }
+        
+        case actionTypes.GET_PRODUCT_FAIL:
+            return {
+                product: [],
+                isLoading: false,
+                error: true
+            }
+
+        default: 
+        return state
+    }
+}
+
+export default productReducer;
