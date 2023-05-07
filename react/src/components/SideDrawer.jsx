bbee6587a42e119dd17307edd20bebd3b7af6c22
Sam Fred <samyung05@gmail.com>
2023-05-07 12:28:10 -0400

    update

--- react/src/components/SideDrawer.jsx
+++ react/src/components/SideDrawer.jsx
@@ -16,5 +16,32 @@ const SideDrawer = ({show, click}) => {
 
     const { cartItems } = cart
 
+    const getCartCount = () =>{
+        return cartItems.reduce((quantity, item) => Number(item.quantity)+quantity , 0)
+      }
     
+      return ( 
+        <div className={sideDrawerClass.join(' ')}>
+          <ul className='sidedrawer__links' onClick={click}>
+    
+            <li>
+              <Link to='/cart'>
+                <i className='fas fa-shopping-cart'/>
+                <span>
+                  Cart <span className='sidedrawer__cartbadge'>{getCartCount()}</span>
+                </span>
+              </Link>
+            </li>
+    
+            <li>
+              <Link to='/'>
+               Shop
+              </Link>
+            </li>
+            
+          </ul>
+        </div>
+      )
 }
+
+export default SideDrawer
