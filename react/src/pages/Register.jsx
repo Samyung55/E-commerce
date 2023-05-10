d9526a56af8229446a9a1c7b4065043ec7bc35b5
Sam Fred <samyung05@gmail.com>
2023-05-10 05:41:57 -0400

    Update Register.jsx

--- react/src/pages/Register.jsx
+++ react/src/pages/Register.jsx
@@ -26,7 +26,19 @@ const RegisterScreen = () => {
     return (
         <div className="registerscreen" onSubmit={handleSubmit(onSubmit)}>
             <form action="" className="registerscreen__form">
-
+                <h2 className="registerscreen__title">Register</h2>
+                <p className="registerscreen__subtext">Already have an account? <Link to='/login'>Login</Link></p>
+                <div className="form__group">
+                    <label htmlFor='username'>Username</label>
+                    <input type='text' name='username' {...register('username', {required: true})} />
+                    {errors.username?.type === 'required' && <span>Username is required</span>}
+                </div>
+                <div className='form__group'>
+                    <lable htmlFor='email'>Email</lable>
+                    <input type='email' name='email' {...register('email', {required:true, pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i})} />
+                    {errors.email?.type === 'required' && <span>Email is required</span>}
+                    {errors.email?.type === 'pattern' && <span>Email is invalid</span>}
+                </div>
             </form>
         </div>
     )
