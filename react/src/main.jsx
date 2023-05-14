2b44de16741af7c79a1b6aa7500de18a00d9c4de
Sam Fred <samyung05@gmail.com>
2023-05-14 15:24:06 -0400

    Update main.jsx

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ProductProvider} from './context/contexts/productContext'
+import { CartProvider } from './context/'
+import {AuthProvider} from './context/reducers/authContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
 )
