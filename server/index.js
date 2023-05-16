677dca6752f61dcdad7df9cf386cdd50503bc371
Sam Fred <samyung05@gmail.com>
2023-05-16 17:55:39 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,57 +1,52 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
-const csurf = require('csurf');
-const mongoose = require('mongoose')
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const dbConnection = require('./db')
-const AppError = require('./utils/AppError')
-
-const errorHandler = require('./middleware/error')
-
-const authRoutes = require('./routes/auth')
+const dbConnection = require('./db');
+const AppError = require('./utils/AppError');
+const errorHandler = require('./middleware/error');
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.URL)
-        console.log("connected to mongodb")
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    await mongoose.connect(process.env.URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 mongoose.connection.on("disconnected", () => {
-    console.log("mongodb disconnected")
-})
-dotenv.config()
+  console.log("MongoDB disconnected");
+});
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173'
-  }));
-  
-const csurfProtection = csurf({
-    cookie: true
-})
+  origin: 'http://localhost:5173'
+}));
 
+const csrfProtection = csrf({ cookie: true });
 
+app.use('/register', csrfProtection);
 
-app.get('/csurf-token', (req, res) => {
-    res.json({ csurfToken: req.csurfToken()})
-})
-app.use(csurfProtection)
-app.all('*', (req, res, next) => {
-    next(new AppError(`Can't find ${reg.originalUrl} on this server!`, 404))
-})
+// Routes go here
+app.use('/register', require('./routes/register')); // Assuming you have a separate file for the '/register' route
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    connect()
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
-
