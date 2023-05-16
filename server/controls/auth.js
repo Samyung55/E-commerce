acbd0667f9a4ed73f1db53eb906fb3ce48183452
Sam Fred <samyung05@gmail.com>
2023-05-16 17:48:40 -0400

    Update auth.js

--- server/controls/auth.js
+++ server/controls/auth.js
@@ -1,131 +1,142 @@
 const express = require('express');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const crypto = require('crypto')
-const User = require('../models/User')
-const AppError = require('../utils/AppError')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const User = require('../models/User');
+const AppError = require('../utils/AppError');
+const csurf = require('csurf');
 
-const sendEmail = require('../utils/sendEmail')
+const sendEmail = require('../utils/sendEmail');
 
 const signToken = (id, email) => {
-    const secret = process.env.JWT_SECRET
-    const payload = {
-        email,
-        id
-    }
+  const secret = process.env.JWT_SECRET;
+  const payload = {
+    email,
+    id
+  };
 
-    return jwt.sign(payload, secret, { expiresIn: '1h'})
-}
+  return jwt.sign(payload, secret, { expiresIn: '1h' });
+};
 
 const createSendToken = (user, statusCode, req, res) => {
-    const token = signToken(user._id, user.email)
-    
-    res.cookie('token', token, {httpOnly: true} )
+  const token = signToken(user._id, user.email);
 
-    //remove password
-    user.password = req.password
+  res.cookie('token', token, { httpOnly: true });
 
-    res.status(statusCode).json({
-        status: 'success',
-        token,
-        data: {
-            user
-        }
-    })
-}
+  // Remove password
+  user.password = req.password;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
 
 const router = express.Router();
+const csrfProtection = csurf({ cookie: true });
 
 router.post('/login', async (req, res, next) => {
-    try {
-        const {email, password} = req.body
-    
-        const existingUser = await User.findOne({ email })
-    
-        if(!existingUser) return next(new AppError('Invalid credentials', 400))
-        
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
-    
-        if(!isPasswordCorrect) return next(new AppError('Invalid credentials', 400))
-    
-        createSendToken(existingUser, 200, req, res)
-    } catch (error) {
-        return next(error)
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return next(new AppError('Invalid credentials', 400));
     }
-})
 
-router.post('/register', async (req, res, next) => {
-    const {username, email, password} = req.body
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
-    const existingUser = await User.findOne({email})
+    if (!isPasswordCorrect) {
+      return next(new AppError('Invalid credentials', 400));
+    }
 
-    if (existingUser) return next(new AppError('Email already in use!', 400))
+    createSendToken(existingUser, 200, req, res);
+  } catch (error) {
+    return next(error);
+  }
+});
 
-    const user = await User.create({
-        username, 
-        email,
-        password
-    })
+router.post('/register', csrfProtection, async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    createSendToken(user, 201, req, res)
-})
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return next(new AppError('Email already in use!', 400));
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    password
+  });
+
+  createSendToken(user, 201, req, res);
+});
 
 router.post('/forgotpassword', async (req, res, next) => {
-    try {
-        const { email } = req.body
-    
-        const user = await User.findOne({ email })
-    
-        if (!user) return next(new AppError('Invalid email', 404))
-        
-        const resetToken = user.createResetPasswordToken()
-        await user.save()
-    
-        const resetUrl = `http://localhost:4000/passwordreset/${resetToken}`
-    
-        const message = `
-           <h2>You had requested for a password reset</h2>
-           <p>Please click the following to reset your password:</p>
-           <a href=${resetUrl} clicktracking=off>ResetLink</a>
-        `
-        await sendEmail({
-            to: user.email,
-            subject: 'Password Reset Request',
-            text: message
-        })
-    
-        res.status(200).json({ success: true, data: 'message sent!'})
-    } catch (error) {
-        return next(error)
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new AppError('Invalid email', 404));
     }
-})
+
+    const resetToken = user.createResetPasswordToken();
+    await user.save();
+
+    const resetUrl = `http://localhost:4000/passwordreset/${resetToken}`;
+
+    const message = `
+      <h2>You had requested for a password reset</h2>
+      <p>Please click the following to reset your password:</p>
+      <a href=${resetUrl} clicktracking=off>ResetLink</a>
+    `;
+    await sendEmail({
+      to: user.email,
+      subject: 'Password Reset Request',
+      text: message
+    });
+
+    res.status(200).json({ success: true, data: 'message sent!' });
+  } catch (error) {
+    return next(error);
+  }
+});
 
 router.post('/resetpassword/:resetToken', async (req, res, next) => {
     try {
-        const resetPasswordToken = crypto
-        .createHash('sha256')
-        .update(req.params.resetToken)
-        .digest('hex')
-    
-        const user = await User.findOne({
-            resetPasswordToken,
-            resetPasswordExpire: { $gt: Date.now() }
-        })
-    
-        if(!user) return next(new AppError('Invalid Token', 400))
-        user.password = req.body.password
-        user.resetPasswordToken = undefined
-        user.resetPasswordExpire = undefined
-    
-        await user.save()
-    
-        res.status(201).json({
-            success: true,
-            data: "Password Updated",
-        })
+    const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resetToken)
+    .digest('hex');
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+      });
+      
+      if (!user) {
+        return next(new AppError('Invalid Token', 400));
+      }
+      user.password = req.body.password;
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+      
+      await user.save();
+      
+      res.status(201).json({
+        success: true,
+        data: 'Password Updated'
+      });
     } catch (error) {
-        return next(error)
+        return next(error);
     }
-})
-
-module.exports = router;
+});
+        
+        module.exports = router;
