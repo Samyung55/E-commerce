669b0f6a98c50250dc695f2435d8121985f8616f
Sam Fred <samyung05@gmail.com>
2023-05-17 18:44:07 -0400

    Update productActions.jsx

--- react/src/context/actions/productActions.jsx
+++ react/src/context/actions/productActions.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const getProducts = async () => {
     try {
 
-        const {data} = await axios.get('http://localhost:4000/auth/products')
+        const {data} = await axios.get('http://localhost:4000/api/products')
 
         return data
 
@@ -15,7 +15,7 @@ export const getProducts = async () => {
 
 export const getProductDetails = async (id) => {
     try {
-        const {data} = await axios.get(`http://localhost:4000/auth/products/${id}`)
+        const {data} = await axios.get(`http://localhost:4000/api/products/${id}`)
 
         return data 
 
