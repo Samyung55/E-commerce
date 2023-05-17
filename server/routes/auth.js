ee1601746323fe6df631c4c4e6b6938a1da62169
Sam Fred <samyung05@gmail.com>
2023-05-17 17:43:30 -0400

    Update auth.js

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { login, register, forgotpassword, resetpassword } = require("../controls/auth")
+
+const router = express.Router()
+
+router.post("/register", register)
+router.post("/login", login)
+router.post("/forgotpassword", login)
+router.post("/resetpassword", resetpassword)
+
+module.exports = router;
