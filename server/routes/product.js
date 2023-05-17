f99b95afe27aaec99ad22b4da0818098685b0b36
Sam Fred <samyung05@gmail.com>
2023-05-17 18:23:46 -0400

    Update product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,7 +4,7 @@ const { products, productsid } = require("../routes/product")
 const router = express.Router()
 
 router.get('/products', products)
-router.get('products/:id', productsid)
+router.get('/products/:id', productsid)
 
 
 module.export = router
