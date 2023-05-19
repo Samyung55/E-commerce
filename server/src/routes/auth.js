d045a2037b1bd4d3c7fb831a98033596ea3fb2af
Sam Fred <samyung05@gmail.com>
2023-05-19 18:48:20 -0400

    commit

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { login, register, forgotpassword, resetpassword } = require("../controls/auth")
+const { login, register, forgotpassword, resetpassword } = require("../controls/auth");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
-router.post("/forgotpassword", login)
-router.post("/resetpassword", resetpassword)
+router.post("/register", register);
+router.post("/login", login);
+router.post("/forgotpassword", forgotpassword);
+router.post("/resetpassword", resetpassword);
 
 module.exports = router;
