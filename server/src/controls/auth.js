233ad14874b45ecba07ec0866a719fb2bdae2e29
Sam Fred <samyung05@gmail.com>
2023-05-19 17:52:22 -0400

    Update auth.js

--- server/src/controls/auth.js
+++ server/src/controls/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const User = require('../../models/User');
+const User = require('../models/User');
 const AppError = require('../utils/AppError');
 const csurf = require('csurf');
 
