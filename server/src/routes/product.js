ac7af40b1cde7f378002be36b616084bfcc0e7b6
Sam Fred <samyung05@gmail.com>
2023-05-21 15:29:04 -0400

    commit

--- server/src/routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { products, productsid } = require("../controls/product")
-
-const router = express.Router()
-
-router.get('/products', products)
-router.get('/products/:id', productsid)
-
-module.exports = router
